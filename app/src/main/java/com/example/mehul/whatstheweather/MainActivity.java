f065db536353ec87b48987d1ec88f32612f8a56e
Mehul Patel <mehulp715@gmail.com>
2017-04-15 09:23:21 -0400

    Add basic onClick functionality

--- app/src/main/java/com/example/mehul/whatstheweather/MainActivity.java
+++ app/src/main/java/com/example/mehul/whatstheweather/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.mehul.whatstheweather;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View v){
+        EditText e = (EditText) findViewById(R.id.editText);
+        Log.i("onClick", "Clicked with value " + e.getText());
     }
 }
